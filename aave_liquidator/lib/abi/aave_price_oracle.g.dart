b9df44e33f1e9493761697f98feb112254bb07b7
bot9 <8978497+seb3n@users.noreply.github.com>
2021-11-02 20:22:35 -0400

    generated abi interface

---
+++ aave_liquidator/lib/abi/aave_price_oracle.g.dart
@@ -0,0 +1,214 @@
+// Generated code, do not modify. Run `build_runner build` to re-generate!
+// @dart=2.12
+import 'package:web3dart/web3dart.dart' as _i1;
+
+final _contractAbi = _i1.ContractAbi.fromJson(
+    '[{"inputs":[{"internalType":"address[]","name":"assets","type":"address[]"},{"internalType":"address[]","name":"sources","type":"address[]"},{"internalType":"address","name":"fallbackOracle","type":"address"},{"internalType":"address","name":"weth","type":"address"}],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"asset","type":"address"},{"indexed":true,"internalType":"address","name":"source","type":"address"}],"name":"AssetSourceUpdated","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"fallbackOracle","type":"address"}],"name":"FallbackOracleUpdated","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"weth","type":"address"}],"name":"WethSet","type":"event"},{"inputs":[],"name":"WETH","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getAssetPrice","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address[]","name":"assets","type":"address[]"}],"name":"getAssetsPrices","outputs":[{"internalType":"uint256[]","name":"","type":"uint256[]"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"getFallbackOracle","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"asset","type":"address"}],"name":"getSourceOfAsset","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address[]","name":"assets","type":"address[]"},{"internalType":"address[]","name":"sources","type":"address[]"}],"name":"setAssetSources","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"fallbackOracle","type":"address"}],"name":"setFallbackOracle","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]',
+    'Aave_price_oracle');
+
+class Aave_price_oracle extends _i1.GeneratedContract {
+  Aave_price_oracle(
+      {required _i1.EthereumAddress address,
+      required _i1.Web3Client client,
+      int? chainId})
+      : super(_i1.DeployedContract(_contractAbi, address), client, chainId);
+
+  /// The optional [atBlock] parameter can be used to view historical data. When
+  /// set, the function will be evaluated in the specified block. By default, the
+  /// latest on-chain block will be used.
+  Future<_i1.EthereumAddress> WETH({_i1.BlockNum? atBlock}) async {
+    final function = self.abi.functions[1];
+    assert(checkSignature(function, 'ad5c4648'));
+    final params = [];
+    final response = await read(function, params, atBlock);
+    return (response[0] as _i1.EthereumAddress);
+  }
+
+  /// The optional [atBlock] parameter can be used to view historical data. When
+  /// set, the function will be evaluated in the specified block. By default, the
+  /// latest on-chain block will be used.
+  Future<BigInt> getAssetPrice(_i1.EthereumAddress asset,
+      {_i1.BlockNum? atBlock}) async {
+    final function = self.abi.functions[2];
+    assert(checkSignature(function, 'b3596f07'));
+    final params = [asset];
+    final response = await read(function, params, atBlock);
+    return (response[0] as BigInt);
+  }
+
+  /// The optional [atBlock] parameter can be used to view historical data. When
+  /// set, the function will be evaluated in the specified block. By default, the
+  /// latest on-chain block will be used.
+  Future<List<BigInt>> getAssetsPrices(List<_i1.EthereumAddress> assets,
+      {_i1.BlockNum? atBlock}) async {
+    final function = self.abi.functions[3];
+    assert(checkSignature(function, '9d23d9f2'));
+    final params = [assets];
+    final response = await read(function, params, atBlock);
+    return (response[0] as List<dynamic>).cast<BigInt>();
+  }
+
+  /// The optional [atBlock] parameter can be used to view historical data. When
+  /// set, the function will be evaluated in the specified block. By default, the
+  /// latest on-chain block will be used.
+  Future<_i1.EthereumAddress> getFallbackOracle({_i1.BlockNum? atBlock}) async {
+    final function = self.abi.functions[4];
+    assert(checkSignature(function, '6210308c'));
+    final params = [];
+    final response = await read(function, params, atBlock);
+    return (response[0] as _i1.EthereumAddress);
+  }
+
+  /// The optional [atBlock] parameter can be used to view historical data. When
+  /// set, the function will be evaluated in the specified block. By default, the
+  /// latest on-chain block will be used.
+  Future<_i1.EthereumAddress> getSourceOfAsset(_i1.EthereumAddress asset,
+      {_i1.BlockNum? atBlock}) async {
+    final function = self.abi.functions[5];
+    assert(checkSignature(function, '92bf2be0'));
+    final params = [asset];
+    final response = await read(function, params, atBlock);
+    return (response[0] as _i1.EthereumAddress);
+  }
+
+  /// The optional [atBlock] parameter can be used to view historical data. When
+  /// set, the function will be evaluated in the specified block. By default, the
+  /// latest on-chain block will be used.
+  Future<_i1.EthereumAddress> owner({_i1.BlockNum? atBlock}) async {
+    final function = self.abi.functions[6];
+    assert(checkSignature(function, '8da5cb5b'));
+    final params = [];
+    final response = await read(function, params, atBlock);
+    return (response[0] as _i1.EthereumAddress);
+  }
+
+  /// The optional [transaction] parameter can be used to override parameters
+  /// like the gas price, nonce and max gas. The `data` and `to` fields will be
+  /// set by the contract.
+  Future<String> renounceOwnership(
+      {required _i1.Credentials credentials,
+      _i1.Transaction? transaction}) async {
+    final function = self.abi.functions[7];
+    assert(checkSignature(function, '715018a6'));
+    final params = [];
+    return write(credentials, transaction, function, params);
+  }
+
+  /// The optional [transaction] parameter can be used to override parameters
+  /// like the gas price, nonce and max gas. The `data` and `to` fields will be
+  /// set by the contract.
+  Future<String> setAssetSources(
+      List<_i1.EthereumAddress> assets, List<_i1.EthereumAddress> sources,
+      {required _i1.Credentials credentials,
+      _i1.Transaction? transaction}) async {
+    final function = self.abi.functions[8];
+    assert(checkSignature(function, 'abfd5310'));
+    final params = [assets, sources];
+    return write(credentials, transaction, function, params);
+  }
+
+  /// The optional [transaction] parameter can be used to override parameters
+  /// like the gas price, nonce and max gas. The `data` and `to` fields will be
+  /// set by the contract.
+  Future<String> setFallbackOracle(_i1.EthereumAddress fallbackOracle,
+      {required _i1.Credentials credentials,
+      _i1.Transaction? transaction}) async {
+    final function = self.abi.functions[9];
+    assert(checkSignature(function, '170aee73'));
+    final params = [fallbackOracle];
+    return write(credentials, transaction, function, params);
+  }
+
+  /// The optional [transaction] parameter can be used to override parameters
+  /// like the gas price, nonce and max gas. The `data` and `to` fields will be
+  /// set by the contract.
+  Future<String> transferOwnership(_i1.EthereumAddress newOwner,
+      {required _i1.Credentials credentials,
+      _i1.Transaction? transaction}) async {
+    final function = self.abi.functions[10];
+    assert(checkSignature(function, 'f2fde38b'));
+    final params = [newOwner];
+    return write(credentials, transaction, function, params);
+  }
+
+  /// Returns a live stream of all AssetSourceUpdated events emitted by this contract.
+  Stream<AssetSourceUpdated> assetSourceUpdatedEvents(
+      {_i1.BlockNum? fromBlock, _i1.BlockNum? toBlock}) {
+    final event = self.event('AssetSourceUpdated');
+    final filter = _i1.FilterOptions.events(
+        contract: self, event: event, fromBlock: fromBlock, toBlock: toBlock);
+    return client.events(filter).map((_i1.FilterEvent result) {
+      final decoded = event.decodeResults(result.topics!, result.data!);
+      return AssetSourceUpdated(decoded);
+    });
+  }
+
+  /// Returns a live stream of all FallbackOracleUpdated events emitted by this contract.
+  Stream<FallbackOracleUpdated> fallbackOracleUpdatedEvents(
+      {_i1.BlockNum? fromBlock, _i1.BlockNum? toBlock}) {
+    final event = self.event('FallbackOracleUpdated');
+    final filter = _i1.FilterOptions.events(
+        contract: self, event: event, fromBlock: fromBlock, toBlock: toBlock);
+    return client.events(filter).map((_i1.FilterEvent result) {
+      final decoded = event.decodeResults(result.topics!, result.data!);
+      return FallbackOracleUpdated(decoded);
+    });
+  }
+
+  /// Returns a live stream of all OwnershipTransferred events emitted by this contract.
+  Stream<OwnershipTransferred> ownershipTransferredEvents(
+      {_i1.BlockNum? fromBlock, _i1.BlockNum? toBlock}) {
+    final event = self.event('OwnershipTransferred');
+    final filter = _i1.FilterOptions.events(
+        contract: self, event: event, fromBlock: fromBlock, toBlock: toBlock);
+    return client.events(filter).map((_i1.FilterEvent result) {
+      final decoded = event.decodeResults(result.topics!, result.data!);
+      return OwnershipTransferred(decoded);
+    });
+  }
+
+  /// Returns a live stream of all WethSet events emitted by this contract.
+  Stream<WethSet> wethSetEvents(
+      {_i1.BlockNum? fromBlock, _i1.BlockNum? toBlock}) {
+    final event = self.event('WethSet');
+    final filter = _i1.FilterOptions.events(
+        contract: self, event: event, fromBlock: fromBlock, toBlock: toBlock);
+    return client.events(filter).map((_i1.FilterEvent result) {
+      final decoded = event.decodeResults(result.topics!, result.data!);
+      return WethSet(decoded);
+    });
+  }
+}
+
+class AssetSourceUpdated {
+  AssetSourceUpdated(List<dynamic> response)
+      : asset = (response[0] as _i1.EthereumAddress),
+        source = (response[1] as _i1.EthereumAddress);
+
+  final _i1.EthereumAddress asset;
+
+  final _i1.EthereumAddress source;
+}
+
+class FallbackOracleUpdated {
+  FallbackOracleUpdated(List<dynamic> response)
+      : fallbackOracle = (response[0] as _i1.EthereumAddress);
+
+  final _i1.EthereumAddress fallbackOracle;
+}
+
+class OwnershipTransferred {
+  OwnershipTransferred(List<dynamic> response)
+      : previousOwner = (response[0] as _i1.EthereumAddress),
+        newOwner = (response[1] as _i1.EthereumAddress);
+
+  final _i1.EthereumAddress previousOwner;
+
+  final _i1.EthereumAddress newOwner;
+}
+
+class WethSet {
+  WethSet(List<dynamic> response) : weth = (response[0] as _i1.EthereumAddress);
+
+  final _i1.EthereumAddress weth;
+}
