845f3a7cc96dfec4a5f9002193aed8e5c54530ff
bot9 <8978497+seb3n@users.noreply.github.com>
2021-11-03 21:22:58 -0400

    MOD: updated db functions; ++ db reserve functions

--- aave_liquidator/lib/services/mongod_service.dart
+++ aave_liquidator/lib/services/mongod_service.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 
 import 'package:aave_liquidator/config.dart';
 import 'package:aave_liquidator/logger.dart';
+import 'package:aave_liquidator/model/aave_reserve_model.dart';
 import 'package:mongo_dart/mongo_dart.dart';
 
 final log = getLogger('MongodService');
@@ -10,25 +11,28 @@ class MongodService {
   MongodService(Config config) {
     _config = config;
     _connectDb();
+    _setupCollections();
   }
   late Config _config;
-  late Db db;
-  late DbCollection userStore;
+  late Db _db;
+  late DbCollection _userStore;
   final Completer<bool> isConnected = Completer<bool>();
+
+  late DbCollection _reserveStore;
   Future<bool> get isReady => isConnected.future;
 
-  /// Connect to db.
+  /// Connect to _db.
   _connectDb() async {
     //TODO: use secure connection.
     log.i('_connectDb');
     try {
-      db = Db(_config.dbUri);
+      _db = Db(_config.dbUri);
 
-      await db.open();
+      await _db.open();
 
-      isConnected.complete(db.isConnected);
+      isConnected.complete(_db.isConnected);
     } catch (e) {
-      log.e('error connecting to db: $e');
+      log.e('error connecting to _db: $e');
     }
   }
 
@@ -36,45 +40,127 @@ class MongodService {
   closeDb() async {
     log.i('closeDb');
     try {
-      await db.close();
+      await _db.close();
     } catch (e) {
-      log.e('error closing db connection: $e');
+      log.e('error closing _db connection: $e');
     }
   }
 
-  /// insert newdocument to db collection.
-  saveUser(Map<String, dynamic> userAccount) {
-    log.i('saveUser | userAccount: $userAccount');
-    userStore = db.collection(_config.aaveUserCollection);
-    userStore.insertOne(userAccount);
+  /// setup collections
+  _setupCollections() async {
+    log.i('_setupCollections');
+    await isReady;
+    _reserveStore = _db.collection(_config.aaveReserveCollectionName);
+    _userStore = _db.collection(_config.aaveUserCollection);
   }
 
-  /// save all users at once.
-  /// WARNING: This will drop the exixtig collection and replace it.
+  /// Insert new users to collection.
+  saveUser(Map<String, dynamic> userAccount) {
+    log.i('saveUser | userAccount: $userAccount');
+
+    _userStore.insertOne(userAccount);
+  }
+
+  /// Reset all users at once.
+  ///
+  /// !!![WARNING]!!!
+  ///
+  /// This will drop the exixting collection and replace it.
+  /// use with caution!!!
   resetUsers(List<Map<String, dynamic>> userList) async {
     log.i('resetUsers | userList: $userList');
     try {
-      await db.dropCollection(_config.aaveUserCollection);
+      await _db.dropCollection(_config.aaveUserCollection);
 
-      userStore = db.collection(_config.aaveUserCollection);
-
-      await userStore.insertMany(userList);
+      await _userStore.insertMany(userList);
     } catch (e) {
-      log.e('error adding user to db :$e');
+      log.e('error adding user to _db :$e');
     }
   }
 
-  /// Replace existing document with new one.
-  /// Add document if does not exist.
-  replaceUserData(Map<String, dynamic> userData) async {
+  /// Update existing user document with new one.
+  /// Adds document if does not exist.
+  updateUser(Map<String, dynamic> userData) async {
     log.i('replaceUserData | userData: $userData');
     try {
-      userStore = db.collection(_config.aaveUserCollection);
-      userStore.replaceOne(
-          where.eq('userAddress', userData['userAddress']), userData,
-          upsert: true);
+      await _userStore.replaceOne(
+        where.eq('userAddress', userData['userAddress']),
+        userData,
+        upsert: true,
+      );
     } catch (e) {
       log.e('error replacing user data: $e');
+    }
+  }
+
+  //
+  //
+  //-------------------RESERVES----------------------///
+  //
+  //
+
+  /// get reserves from _db
+  ///
+  Future<List<AaveReserveData>> getReservesFromDb() async {
+    log.i('getReservesFromDb');
+    try {
+      var res = _reserveStore.find();
+      return res.map((event) => _parseReserveToAaveReserveData(event)).toList();
+    } catch (e) {
+      log.e('error getting reservesfrom db: $e');
+      throw '';
+    }
+  }
+
+  /// parse reservedata.
+  AaveReserveData _parseReserveToAaveReserveData(data) {
+    log.v('_parseReserveToAaveReserveData');
+    return AaveReserveData(
+      assetAddress: data['assetAddress'],
+      assetConfig: data['assetConfiguration'],
+      assetPrice: data['assetPrice'],
+      assetPriceETH: data['assetPriceEth'],
+      aaveAssetPrice: data['aaveAssetPrice'],
+    );
+  }
+
+  Future<bool> resetReserveData(List<AaveReserveData> reserveDataList) async {
+    log.i('resetReserveData');
+    await _reserveStore.drop();
+    List<Map<String, dynamic>> documents =
+        reserveDataList.map((e) => e.toJson()).toList();
+
+    var res = await _reserveStore.insertMany(documents);
+    return res.success;
+  }
+
+  /// Updates the asset price with price from chainlink
+  updateReserveAssetPrice({
+    required String assetAddress,
+    required double newAssetPrice,
+  }) async {
+    try {
+      final reserveStore = _db.collection(_config.aaveReserveCollectionName);
+      await reserveStore.updateOne(
+        where.eq('reserveAddress', assetAddress),
+        modify.set('assetPrice', newAssetPrice),
+      );
+    } catch (e) {
+      log.e('error updating asset price: $e');
+    }
+  }
+
+  /// Updates all data of a given asset reserve.
+  updateAaveReserve(AaveReserveData reserveData) async {
+    try {
+      final reserveStore = _db.collection(_config.aaveReserveCollectionName);
+      await reserveStore.replaceOne(
+        where.eq('reserveAddress', reserveData.assetAddress),
+        reserveData.toJson(),
+        upsert: true,
+      );
+    } catch (e) {
+      log.e('error updating reserve: $e');
     }
   }
 }
