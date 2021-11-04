8e6cf622c841770e9a8135afc56ce26e9551a9ca
bot9 <8978497+seb3n@users.noreply.github.com>
2021-11-03 21:26:10 -0400

    MOD:  tie it all togeter

--- aave_liquidator/bin/aave_liquidator.dart
+++ aave_liquidator/bin/aave_liquidator.dart
@@ -1,9 +1,15 @@
 import 'package:aave_liquidator/config.dart';
+import 'package:aave_liquidator/contract_helpers/chainlink_contracts.dart';
+import 'package:aave_liquidator/contract_interface/aave_reserve_manager.dart';
+import 'package:aave_liquidator/contract_interface/aave_user_manager.dart';
+import 'package:aave_liquidator/contract_interface/chain_link_interface.dart';
 import 'package:aave_liquidator/logger.dart';
+import 'package:aave_liquidator/model/aave_reserve_model.dart';
 import 'package:aave_liquidator/services/mongod_service.dart';
 import 'package:aave_liquidator/services/web3_service.dart';
 import 'package:dotenv/dotenv.dart';
 import 'package:logger/logger.dart';
+import 'package:aave_liquidator/contract_helpers/aave_contracts.dart';
 
 final log = getLogger('main');
 void main() async {
@@ -15,22 +21,108 @@ void main() async {
   final Config _config = Config();
 
   /// Connect to db.
-  MongodService mongod = MongodService(_config);
-  await mongod.isReady;
+  final MongodService _mongodService = MongodService(_config);
+  await _mongodService.isReady;
 
   /// Connect to blockchain network via infura
-  Web3Service _web3 = Web3Service(_config, mongod);
+  final Web3Service _web3 = Web3Service(_config, _mongodService);
   await _web3.isReady;
 
-  /// EVERY 24hrs
-  /// get reserve list => store in db
-  /// get reserve config data => store in db
-  /// get new users by querying past borrow events
-  /// update known users
+  /// Setup aave contracts
+  final AaveContracts _aaveContracts = AaveContracts(_web3, _config);
 
-  /// every 30 min,
-  /// get assets price
-  /// convert price in ETH
+  /// setup chainlink contracts
+  final ChainlinkContracts _chainlinkContracts =
+      ChainlinkContracts(_web3, _config);
+
+  final ChainLinkPriceOracle _oracle = ChainLinkPriceOracle(
+    chainlinkContracts: _chainlinkContracts,
+    config: _config,
+    mongod: _mongodService,
+  );
+
+  final AaveReserveManager _reserveManager = AaveReserveManager(
+    config: _config,
+    mongod: _mongodService,
+    aaveContracts: _aaveContracts,
+  );
+
+  /// Poll Aave reserves
+  ///
+  /// This is a scheduled task to update reserve data periodically
+  /// TODO: create 24hr cron repeat interval.
+
+  _pollReserveData() async {
+    log.i('_pollReserveData');
+
+    /// get reserve data from db.
+    final List<AaveReserveData> reserveDataList =
+        await _mongodService.getReservesFromDb();
+    log.d('reserve list: $reserveDataList');
+
+    /// get list of assets from aave.
+    final _assetsList = await _reserveManager.getAaveReserveList();
+
+    /// get price from aave
+    final _assetPriceList =
+        await _reserveManager.getAllReserveAssetPrice(_assetsList);
+    log.d('asset price list: $_assetPriceList');
+
+    /// get asset config from aave
+    final _assetConfigList =
+        await _reserveManager.getAllReserveAssetConfigData(_assetsList);
+    log.d('asset config list: $_assetConfigList');
+
+    /// get asset price from chainlink
+    final List<Map<String, double>> _oracleAssetPriceList =
+        _oracle.getAllAssetsPrice(_assetsList);
+    log.d('oracle pricelist: $_oracleAssetPriceList');
+
+    /// update [reserveData] with new data
+    for (int i = 0; i < _assetsList.length; i++) {
+      int index = reserveDataList.indexWhere(
+          (element) => element.assetAddress == _assetsList[i].toString());
+      if (index == -1) {
+        reserveDataList.add(AaveReserveData(
+          assetAddress: _assetsList[i].toString(),
+          assetConfig: _assetConfigList[i],
+          aaveAssetPrice: _assetPriceList[i],
+          assetPrice: _oracleAssetPriceList.first['USD']!,
+          assetPriceETH: _oracleAssetPriceList.first['ETH']!,
+        ));
+      } else {
+        reserveDataList[index] = AaveReserveData(
+          assetAddress: _assetsList[i].toString(),
+          assetConfig: _assetConfigList[i],
+          aaveAssetPrice: _assetPriceList[i],
+          assetPrice: _oracleAssetPriceList[i]['USD']!,
+          assetPriceETH: _oracleAssetPriceList[i]['ETH']!,
+        );
+      }
+    }
+
+    /// update db with new reserve data.
+    await _mongodService.resetReserveData(reserveDataList);
+  }
+
+  await _pollReserveData();
+
+  // get new users by querying past borrow events
+  // update known users
+
+  // every 30 min,
+  // get assets price
+  // convert price in ETH
+
+  // Listens for asset price change
+  // update new price in db
+  // _oracle.listenForEthPriceUpdate().onData((data) {
+  //   print('data received');
+
+  //   _mongodService.updateReserveAssetPrice(
+  //       assetAddress: _config.ethTokenAddress,
+  //       newAssetPrice: data.current.toDouble());
+  // });
 
   /// for every asset available on aave
   /// listen for price emmit
@@ -44,8 +136,10 @@ void main() async {
   /// if price % change is < than aave price discovery threshold
   /// update user account data
   /// update price from aave
+  // final AaveUserManager _userManager =
+  //     AaveUserManager(web3: _web3, config: _config, mongod: _mongod);
 
   /// Terminate all conections
   _web3.dispose();
-  mongod.closeDb();
+  _mongodService.closeDb();
 }
